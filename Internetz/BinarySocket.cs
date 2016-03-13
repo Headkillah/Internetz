8ec7187cba6f4df67d02c54d25f708abebfe1bb5
Saša Barišić <cartman300@net.hr>
2016-03-13 16:30:17 +0100

    Project files

---
+++ Internetz/BinarySocket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Internetz {
+	public class BinarySocket : Socket {
+		public BinarySocket(AddressFamily AddrFam, SocketType SType, ProtocolType PType) : base(AddrFam, SType, PType) {
+		}
+
+		public BinarySocket(SocketType SType, ProtocolType PType) : this(AddressFamily.InterNetwork, SType, PType) {
+		}
+
+		public BinarySocket(ProtocolType PType) :this(SocketType.Stream, PType) {
+		}
+
+		public bool TryConnect(IPAddress Addr, int Port) {
+			try {
+				Connect(Addr, Port);
+			} catch (SocketException) {
+				return false;
+			}
+			return true;
+		}
+	}
+}
---
+++ Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
+using Internetz;
+
+namespace Test {
+	class Program {
+		static void Main(string[] args) {
+			Console.Title = "Internetz Test";
+
+			Thread ServerThread = new Thread(() => Server());
+			Thread ClientThread = new Thread(() => Client());
+			ServerThread.Start();
+			ClientThread.Start();
+
+			while (ServerThread.ThreadState == ThreadState.Running || ClientThread.ThreadState == ThreadState.Running)
+				Thread.Sleep(50);
+
+			Console.WriteLine("Done!");
+			Console.ReadLine();
+		}
+
+		static void Server() {
+			BinarySocket S = new BinarySocket(ProtocolType.Tcp);
+		}
+
+		static void Client() {
+			BinarySocket C = new BinarySocket(ProtocolType.Tcp);
+		}
+	}
+}
